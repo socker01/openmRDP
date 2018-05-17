11cd1c90e6f583ba8e3b74b53e417fde7f77b752
socker <jk.socker@gmail.com>
2018-05-17 19:03:22 +0200

    Fix SecurityConfigurationFactory error

--- src/cz/vutbr/fit/openmrdp/security/SecurityConfigurationFactory.java
+++ src/cz/vutbr/fit/openmrdp/security/SecurityConfigurationFactory.java
@@ -3,16 +3,22 @@ package cz.vutbr.fit.openmrdp.security;
 import com.sun.istack.internal.NotNull;
 
 /**
+ * Factory for creating of SecurityConfiguration.
+ *
  * @author Jiri Koudelka
  * @since 07.05.2018
  */
 public final class SecurityConfigurationFactory {
 
-    public static SecurityConfiguration createSecureSecurityConfiguration(@NotNull UserAuthorizator userAuthorizator){
-        return new SecurityConfiguration(true, userAuthorizator);
+    public static SecurityConfiguration createSecureSecurityConfiguration(@NotNull UserAuthorizator userAuthorizator) {
+        return new SecurityConfiguration(true, userAuthorizator, false);
     }
 
-    public static SecurityConfiguration createNonSecureSecurityConfiguration(){
-        return new SecurityConfiguration(false, null);
+    public static SecurityConfiguration createDebugSecureSecurityConfiguration(@NotNull UserAuthorizator userAuthorizator) {
+        return new SecurityConfiguration(true, userAuthorizator, true);
+    }
+
+    public static SecurityConfiguration createNonSecureSecurityConfiguration() {
+        return new SecurityConfiguration(false, null, false);
     }
 }
